5b9d02567f451a4d006cfa50e0ba9d5e5db70070
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-25 18:53:08 +0900

    useEffectの設定

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ const App = () => {
 
   const [images, setImages] = useState([]); //何の写真が表示されているか管理
   const [text, setText] = useState(''); //検索バーに入れる文字列の管理
-  const [query, setQuery] = useState('cat'); //検索されている文字列の保持
+  const [query, setQuery] = useState(''); //検索されている文字列の保持
+
+  useEffect(() => {
+    console.log('useEffectが走りました。')
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID_UNSPLASH}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setImages(data.results)
+      })
+  }, [query])//queryが変わった時にuseEffectを走らせる
 
   const onClickSearch = (e) => {
     e.preventDefault(); //ページ遷移の防止（SPAのため）
     setQuery(text);
     setText('');
     console.log(`${text}でonClickSearchが呼ばれました`)
+    console.log(query)
   }
 
   return (
