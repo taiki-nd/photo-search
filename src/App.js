c6f662ac6e7cd32f46ee20d6323e4d36b09f7d12
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-25 17:35:11 +0900

    stateの設定

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const App = () => {
+
+  const [images, setImages] = useState([]); //何の写真が表示されているか管理
+  const [text, setText] = useState(''); //検索バーに入れる文字列の管理
+  const [query, setQuery] = useState('cat'); //検索されている文字列の保持
+
   return (
     <div>
       <h1>Hello World</h1>
