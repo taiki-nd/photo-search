1a6085a6ff22b72cfe3fadcd19563666881660a5
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-28 19:53:19 +0900

    routerの導入

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Unsplash } from './components/unsplash';
 import { Pixabay } from './components/pixabay';
 
