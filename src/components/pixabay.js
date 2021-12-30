9482f479b241f143da9aa1c2603ac1fc46abf348
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-30 21:28:17 +0900

    pixabayの修正

--- src/components/pixabay.js
+++ src/components/pixabay.js
@@ -18,7 +18,6 @@ export const Pixabay = (props) => {
 
   return (
     <>
-      <LetSearch>"{query}"の検索結果 form pixabay</LetSearch>
       <Photos>
         {images_p.map(image => (
           <Card key={image.id}>
@@ -31,22 +30,11 @@ export const Pixabay = (props) => {
   )
 }
 
-const LetSearch = styled.p`
-  font-size: 1.5rem;
-  font-weight: bold;
-  text-align: center;
-  color: #ffffff;
-`
-
-const Content = styled.div`
-  width: 90%;
-  margin: 0 auto;
-`
-
 const Photos = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   gap: 25px;
+  place-items: center;
 `
 const Card = styled.div`
   width: 300px;
