64d3ecad6bf4b9027b509a6370d774284cf11e1f
Taiki Noda <taiki.nd.real@gmail.com>
2021-12-31 20:45:50 +0900

    meta tagの設定

--- src/components/head.js
+++ src/components/head.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import BackImage from '../images/back.jpg';
 
 export const Head = () => {
 
@@ -8,6 +9,19 @@ export const Head = () => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>Photo Search</title>
+        <meta
+          name="Photo Search"
+          content="無料で高品質な商用利用可能の写真を探す"
+        />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0"></meta>
+
+        <head prefix="og: http://ogp.me/ns#" />
+        <meta property="og:url" content="https://photo-search-0517.herokuapp.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Photo Search" />
+        <meta property="og:description" content="無料で高品質な商用利用可能の写真を探す" />
+        <meta property="og:site_name" content="Photo Search" />
+        <meta property="og:image" content="https://raw.githubusercontent.com/taiki-nd/photo-search/master/src/images/back.jpg" />
       </Helmet>
     </>
   )
